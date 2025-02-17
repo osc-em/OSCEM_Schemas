70e4c597befdc0b45902dd4dbe10e64cc053a82e
Yves Ulrich Tittes <yves.tittes@unibas.ch>
2025-02-17 15:08:56 +0100

    updated descriptor to allow "Any"

--- project/sqlschema/oscem_schemas.sql
+++ project/sqlschema/oscem_schemas.sql
@@ -3,6 +3,7 @@
 --     * Slot: acquisition_id Description: Describe the data acquisition parameters
 --     * Slot: instrument_id Description: Describe the instrument used to acquire the data
 --     * Slot: sample_id Description: Sample information
+--     * Slot: processing_id Description: Describe the processing done to the data
 -- # Class: "TiltAngle" Description: "The min, max and increment of the tilt angle in a tomography session. Unit is degree."
 --     * Slot: id Description: 
 --     * Slot: increment_id Description: Increment between elements of a series
@@ -33,6 +34,14 @@
 --     * Slot: id Description: 
 --     * Slot: unit Description: the unit of a given value
 --     * Slot: value Description: the value of a field with a unit
+-- # Class: "Descriptor" Description: "List of custom descriptors for user-defined key-value pairs describing how micrographs were obtained or any related information"
+--     * Slot: id Description: 
+--     * Slot: descriptor_name Description: Name defining the descriptor
+--     * Slot: descriptor_thing_id Description: Description of the descriptor
+-- # Class: "Descriptors" Description: ""
+--     * Slot: id Description: 
+--     * Slot: descriptor_name Description: Name defining the descriptor
+--     * Slot: descriptor_thing_id Description: Description of the descriptor
 -- # Class: "Acquisition" Description: "A set of parameteres describing the data acquisition"
 --     * Slot: id Description: 
 --     * Slot: nominal_magnification Description: Magnification level as indicated by the instrument, no unit
@@ -174,6 +183,84 @@
 --     * Slot: overall_molecule_id Description: Description of the overall molecule
 --     * Slot: specimen_id Description: Description of the specimen
 --     * Slot: grid_id Description: Description of the grid used
+-- # Class: "Movies" Description: "Class representing movies metadata"
+--     * Slot: id Description: 
+--     * Slot: gain_image Description: Gain image filename
+--     * Slot: dark_image Description: Dark image filename
+--     * Slot: dose_per_image_id Description: Dose per image
+--     * Slot: initial_dose_id Description: Initial dose
+-- # Class: "Micrographs" Description: "Class representing micrographs metadata"
+--     * Slot: id Description: 
+--     * Slot: number_micrographs Description: Number of micrographs
+-- # Class: "CTFs" Description: "Class representing Contrast Transfer Function (CTF) metadata"
+--     * Slot: id Description: 
+--     * Slot: amplitude_contrast Description: Amplitude contrast
+--     * Slot: defocus_id Description: Defocus metadata
+--     * Slot: resolution_id Description: Resolution metadata
+--     * Slot: astigmatism_id Description: Astigmatism metadata
+-- # Class: "Defocus" Description: "Defocus-related metadata"
+--     * Slot: id Description: 
+--     * Slot: defocus_histogram Description: Filename of the defocus values histogram
+--     * Slot: defocus_micrograph_examples Description: Filename of micrographs shown as examples in ascending order of defocus value
+--     * Slot: output_min_defocus_id Description: Minimum defocus
+--     * Slot: output_max_defocus_id Description: Maximum defocus
+--     * Slot: output_avg_defocus_id Description: Average value of defocus
+-- # Class: "Resolution" Description: "Resolution-related metadata"
+--     * Slot: id Description: 
+--     * Slot: resolution_histogram Description: Filename of the resolution values histogram
+--     * Slot: output_min_resolution_id Description: Minimum resolution
+--     * Slot: output_max_resolution_id Description: Maximum resolution
+--     * Slot: output_avg_resolution_id Description: Average value of resolution
+-- # Class: "Astigmatism" Description: "Astigmatism-related metadata"
+--     * Slot: id Description: 
+--     * Slot: astigmatism_histogram Description: Filename of the astigmatism values histogram
+-- # Class: "Coordinates" Description: "Class representing coordinates metadata"
+--     * Slot: id Description: 
+--     * Slot: number_particles Description: Total number of particles
+--     * Slot: particles_per_micrograph Description: Mean number of particles per micrograph
+--     * Slot: num_source_micrographs Description: Total number of micrographs from which coordinates come from
+--     * Slot: particles_histogram Description: Filename of histogram for particle number per micrograph
+--     * Slot: micrograph_examples Description: Filename of micrographs shown as examples in descending order based on the number of particles
+-- # Class: "Classes2D" Description: "Class representing classes 2D metadata"
+--     * Slot: id Description: 
+--     * Slot: number_classes_2D Description: Number of 2D classes
+--     * Slot: images_classes_2D Description: Filename of the image containing 2D class images
+--     * Slot: resolution_classes_2D_id Description: Resolution of classes 2D
+-- # Class: "Classes3D" Description: "Class representing classes 3D metadata"
+--     * Slot: id Description: 
+--     * Slot: number_classes_3D Description: Number of 3D classes
+--     * Slot: images_classes_3D Description: Filename of the image containing 3D class images
+--     * Slot: resolution_classes_3D_id Description: Resolution of volume
+-- # Class: "Volume" Description: "Class describing volume(s) obtained"
+--     * Slot: id Description: 
+--     * Slot: orthogonal_slices_id Description: orthogonal slices of volume
+--     * Slot: isosurface_images_id Description: isosurface images of volume
+-- # Class: "OrthogonalSlices" Description: "Class for the orthogonal slices"
+--     * Slot: id Description: 
+--     * Slot: orthogonal_slices_X Description: Filename of orthogonal slices in X direction
+--     * Slot: orthogonal_slices_Y Description: Filename of orthogonal slices in Y direction
+--     * Slot: orthogonal_slices_Z Description: Filename of orthogonal slices in Z direction
+-- # Class: "IsosurfaceImages" Description: "Class for the isosurface images"
+--     * Slot: id Description: 
+--     * Slot: front_view Description: Filename of the front view isosurface image
+--     * Slot: side_view Description: Filename of the side view isosurface image
+--     * Slot: top_view Description: Filename of the top view isosurface image
+-- # Class: "Volumes" Description: "Class representing volume metadata"
+--     * Slot: id Description: 
+--     * Slot: volume_type Description: Indicates the type of volume
+--     * Slot: vol_number_particles Description: Number of particles associated to volume
+--     * Slot: size Description: Size of the volume
+--     * Slot: orthogonal_slices_id Description: orthogonal slices of volume
+--     * Slot: isosurface_images_id Description: isosurface images of volume
+--     * Slot: vol_resolution_id Description: Resolution of volume
+-- # Class: "Processing" Description: "Set of parameters describing the data processing"
+--     * Slot: id Description: 
+--     * Slot: movies_id Description: Movies metadata
+--     * Slot: micrographs_id Description: Micrographs metadata
+--     * Slot: ctfs_id Description: CTFs metadata
+--     * Slot: coordinates_id Description: Coordinates metadata
+--     * Slot: classes2D_id Description: Classes 2D metadata
+--     * Slot: classes3D_id Description: Classes 3D metadata
 -- # Class: "EMDataset_grants" Description: ""
 --     * Slot: EMDataset_id Description: Autocreated FK slot
 --     * Slot: grants_id Description: List of grants associated with the project
@@ -186,6 +273,39 @@
 -- # Class: "Sample_ligands" Description: ""
 --     * Slot: Sample_id Description: Autocreated FK slot
 --     * Slot: ligands_id Description: List of ligands associated with the sample
+-- # Class: "Movies_descriptors" Description: ""
+--     * Slot: Movies_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "Micrographs_descriptors" Description: ""
+--     * Slot: Micrographs_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "CTFs_descriptors" Description: ""
+--     * Slot: CTFs_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "Coordinates_descriptors" Description: ""
+--     * Slot: Coordinates_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "Classes2D_particles_per_2Dclass" Description: ""
+--     * Slot: Classes2D_id Description: Autocreated FK slot
+--     * Slot: particles_per_2Dclass Description: Number of particles per 2D class
+-- # Class: "Classes2D_descriptors" Description: ""
+--     * Slot: Classes2D_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "Classes3D_particles_per_3Dclass" Description: ""
+--     * Slot: Classes3D_id Description: Autocreated FK slot
+--     * Slot: particles_per_3Dclass Description: Number of particles per 3D class
+-- # Class: "Classes3D_volume" Description: ""
+--     * Slot: Classes3D_id Description: Autocreated FK slot
+--     * Slot: volume_id Description: Describes volume(s) obtained
+-- # Class: "Classes3D_descriptors" Description: ""
+--     * Slot: Classes3D_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "Volumes_descriptors" Description: ""
+--     * Slot: Volumes_id Description: Autocreated FK slot
+--     * Slot: descriptors_id Description: List of custom descriptors for user-defined key-value pairs describing how movies were obtained or any related information
+-- # Class: "Processing_volumes" Description: ""
+--     * Slot: Processing_id Description: Autocreated FK slot
+--     * Slot: volumes_id Description: Volume metadata
 
 CREATE TABLE "Any" (
 	id INTEGER NOT NULL, 
@@ -271,6 +391,39 @@ CREATE TABLE "Ligand" (
 	reference TEXT, 
 	PRIMARY KEY (id)
 );
+CREATE TABLE "Micrographs" (
+	id INTEGER NOT NULL, 
+	number_micrographs FLOAT NOT NULL, 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "Astigmatism" (
+	id INTEGER NOT NULL, 
+	astigmatism_histogram TEXT, 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "Coordinates" (
+	id INTEGER NOT NULL, 
+	number_particles INTEGER NOT NULL, 
+	particles_per_micrograph FLOAT, 
+	num_source_micrographs INTEGER, 
+	particles_histogram TEXT, 
+	micrograph_examples TEXT, 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "OrthogonalSlices" (
+	id INTEGER NOT NULL, 
+	"orthogonal_slices_X" TEXT, 
+	"orthogonal_slices_Y" TEXT, 
+	"orthogonal_slices_Z" TEXT, 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "IsosurfaceImages" (
+	id INTEGER NOT NULL, 
+	front_view TEXT, 
+	side_view TEXT, 
+	top_view TEXT, 
+	PRIMARY KEY (id)
+);
 CREATE TABLE "TiltAngle" (
 	id INTEGER NOT NULL, 
 	increment_id INTEGER, 
@@ -310,6 +463,20 @@ CREATE TABLE "BoundingBox2D" (
 	FOREIGN KEY(x_max_id) REFERENCES "QuantityValue" (id), 
 	FOREIGN KEY(y_min_id) REFERENCES "QuantityValue" (id), 
 	FOREIGN KEY(y_max_id) REFERENCES "QuantityValue" (id)
+);
+CREATE TABLE "Descriptor" (
+	id INTEGER NOT NULL, 
+	descriptor_name TEXT NOT NULL, 
+	descriptor_thing_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(descriptor_thing_id) REFERENCES "Any" (id)
+);
+CREATE TABLE "Descriptors" (
+	id INTEGER NOT NULL, 
+	descriptor_name TEXT NOT NULL, 
+	descriptor_thing_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(descriptor_thing_id) REFERENCES "Any" (id)
 );
 CREATE TABLE "SpecialistOptics" (
 	id INTEGER NOT NULL, 
@@ -391,6 +558,76 @@ CREATE TABLE "Grid" (
 	FOREIGN KEY(pretreatment_time_id) REFERENCES "QuantityValue" (id), 
 	FOREIGN KEY(pretreatment_pressure_id) REFERENCES "QuantityValue" (id)
 );
+CREATE TABLE "Movies" (
+	id INTEGER NOT NULL, 
+	gain_image TEXT, 
+	dark_image TEXT, 
+	dose_per_image_id INTEGER, 
+	initial_dose_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(dose_per_image_id) REFERENCES "QuantityValue" (id), 
+	FOREIGN KEY(initial_dose_id) REFERENCES "QuantityValue" (id)
+);
+CREATE TABLE "Defocus" (
+	id INTEGER NOT NULL, 
+	defocus_histogram TEXT, 
+	defocus_micrograph_examples TEXT, 
+	output_min_defocus_id INTEGER, 
+	output_max_defocus_id INTEGER, 
+	output_avg_defocus_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(output_min_defocus_id) REFERENCES "QuantityValue" (id), 
+	FOREIGN KEY(output_max_defocus_id) REFERENCES "QuantityValue" (id), 
+	FOREIGN KEY(output_avg_defocus_id) REFERENCES "QuantityValue" (id)
+);
+CREATE TABLE "Resolution" (
+	id INTEGER NOT NULL, 
+	resolution_histogram TEXT, 
+	output_min_resolution_id INTEGER, 
+	output_max_resolution_id INTEGER, 
+	output_avg_resolution_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(output_min_resolution_id) REFERENCES "QuantityValue" (id), 
+	FOREIGN KEY(output_max_resolution_id) REFERENCES "QuantityValue" (id), 
+	FOREIGN KEY(output_avg_resolution_id) REFERENCES "QuantityValue" (id)
+);
+CREATE TABLE "Classes2D" (
+	id INTEGER NOT NULL, 
+	"number_classes_2D" INTEGER, 
+	"images_classes_2D" TEXT, 
+	"resolution_classes_2D_id" INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY("resolution_classes_2D_id") REFERENCES "QuantityValue" (id)
+);
+CREATE TABLE "Classes3D" (
+	id INTEGER NOT NULL, 
+	"number_classes_3D" INTEGER, 
+	"images_classes_3D" TEXT, 
+	"resolution_classes_3D_id" INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY("resolution_classes_3D_id") REFERENCES "QuantityValue" (id)
+);
+CREATE TABLE "Volume" (
+	id INTEGER NOT NULL, 
+	orthogonal_slices_id INTEGER, 
+	isosurface_images_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(orthogonal_slices_id) REFERENCES "OrthogonalSlices" (id), 
+	FOREIGN KEY(isosurface_images_id) REFERENCES "IsosurfaceImages" (id)
+);
+CREATE TABLE "Volumes" (
+	id INTEGER NOT NULL, 
+	volume_type TEXT NOT NULL, 
+	vol_number_particles INTEGER, 
+	size TEXT, 
+	orthogonal_slices_id INTEGER, 
+	isosurface_images_id INTEGER, 
+	vol_resolution_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(orthogonal_slices_id) REFERENCES "OrthogonalSlices" (id), 
+	FOREIGN KEY(isosurface_images_id) REFERENCES "IsosurfaceImages" (id), 
+	FOREIGN KEY(vol_resolution_id) REFERENCES "QuantityValue" (id)
+);
 CREATE TABLE "Acquisition" (
 	id INTEGER NOT NULL, 
 	nominal_magnification INTEGER, 
@@ -445,15 +682,93 @@ CREATE TABLE "Sample" (
 	FOREIGN KEY(specimen_id) REFERENCES "Specimen" (id), 
 	FOREIGN KEY(grid_id) REFERENCES "Grid" (id)
 );
-CREATE TABLE "EMDataset" (
+CREATE TABLE "CTFs" (
 	id INTEGER NOT NULL, 
-	acquisition_id INTEGER NOT NULL, 
-	instrument_id INTEGER NOT NULL, 
-	sample_id INTEGER NOT NULL, 
+	amplitude_contrast FLOAT, 
+	defocus_id INTEGER, 
+	resolution_id INTEGER, 
+	astigmatism_id INTEGER, 
 	PRIMARY KEY (id), 
-	FOREIGN KEY(acquisition_id) REFERENCES "Acquisition" (id), 
-	FOREIGN KEY(instrument_id) REFERENCES "Instrument" (id), 
-	FOREIGN KEY(sample_id) REFERENCES "Sample" (id)
+	FOREIGN KEY(defocus_id) REFERENCES "Defocus" (id), 
+	FOREIGN KEY(resolution_id) REFERENCES "Resolution" (id), 
+	FOREIGN KEY(astigmatism_id) REFERENCES "Astigmatism" (id)
+);
+CREATE TABLE "Movies_descriptors" (
+	"Movies_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("Movies_id", descriptors_id), 
+	FOREIGN KEY("Movies_id") REFERENCES "Movies" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "Micrographs_descriptors" (
+	"Micrographs_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("Micrographs_id", descriptors_id), 
+	FOREIGN KEY("Micrographs_id") REFERENCES "Micrographs" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "Coordinates_descriptors" (
+	"Coordinates_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("Coordinates_id", descriptors_id), 
+	FOREIGN KEY("Coordinates_id") REFERENCES "Coordinates" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "Classes2D_particles_per_2Dclass" (
+	"Classes2D_id" INTEGER, 
+	"particles_per_2Dclass" INTEGER, 
+	PRIMARY KEY ("Classes2D_id", "particles_per_2Dclass"), 
+	FOREIGN KEY("Classes2D_id") REFERENCES "Classes2D" (id)
+);
+CREATE TABLE "Classes2D_descriptors" (
+	"Classes2D_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("Classes2D_id", descriptors_id), 
+	FOREIGN KEY("Classes2D_id") REFERENCES "Classes2D" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "Classes3D_particles_per_3Dclass" (
+	"Classes3D_id" INTEGER, 
+	"particles_per_3Dclass" INTEGER, 
+	PRIMARY KEY ("Classes3D_id", "particles_per_3Dclass"), 
+	FOREIGN KEY("Classes3D_id") REFERENCES "Classes3D" (id)
+);
+CREATE TABLE "Classes3D_volume" (
+	"Classes3D_id" INTEGER, 
+	volume_id INTEGER, 
+	PRIMARY KEY ("Classes3D_id", volume_id), 
+	FOREIGN KEY("Classes3D_id") REFERENCES "Classes3D" (id), 
+	FOREIGN KEY(volume_id) REFERENCES "Volume" (id)
+);
+CREATE TABLE "Classes3D_descriptors" (
+	"Classes3D_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("Classes3D_id", descriptors_id), 
+	FOREIGN KEY("Classes3D_id") REFERENCES "Classes3D" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "Volumes_descriptors" (
+	"Volumes_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("Volumes_id", descriptors_id), 
+	FOREIGN KEY("Volumes_id") REFERENCES "Volumes" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "Processing" (
+	id INTEGER NOT NULL, 
+	movies_id INTEGER, 
+	micrographs_id INTEGER, 
+	ctfs_id INTEGER, 
+	coordinates_id INTEGER, 
+	"classes2D_id" INTEGER, 
+	"classes3D_id" INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(movies_id) REFERENCES "Movies" (id), 
+	FOREIGN KEY(micrographs_id) REFERENCES "Micrographs" (id), 
+	FOREIGN KEY(ctfs_id) REFERENCES "CTFs" (id), 
+	FOREIGN KEY(coordinates_id) REFERENCES "Coordinates" (id), 
+	FOREIGN KEY("classes2D_id") REFERENCES "Classes2D" (id), 
+	FOREIGN KEY("classes3D_id") REFERENCES "Classes3D" (id)
 );
 CREATE TABLE "Sample_molecule" (
 	"Sample_id" INTEGER, 
@@ -468,6 +783,32 @@ CREATE TABLE "Sample_ligands" (
 	PRIMARY KEY ("Sample_id", ligands_id), 
 	FOREIGN KEY("Sample_id") REFERENCES "Sample" (id), 
 	FOREIGN KEY(ligands_id) REFERENCES "Ligand" (id)
+);
+CREATE TABLE "CTFs_descriptors" (
+	"CTFs_id" INTEGER, 
+	descriptors_id INTEGER, 
+	PRIMARY KEY ("CTFs_id", descriptors_id), 
+	FOREIGN KEY("CTFs_id") REFERENCES "CTFs" (id), 
+	FOREIGN KEY(descriptors_id) REFERENCES "Descriptors" (id)
+);
+CREATE TABLE "EMDataset" (
+	id INTEGER NOT NULL, 
+	acquisition_id INTEGER NOT NULL, 
+	instrument_id INTEGER NOT NULL, 
+	sample_id INTEGER NOT NULL, 
+	processing_id INTEGER NOT NULL, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(acquisition_id) REFERENCES "Acquisition" (id), 
+	FOREIGN KEY(instrument_id) REFERENCES "Instrument" (id), 
+	FOREIGN KEY(sample_id) REFERENCES "Sample" (id), 
+	FOREIGN KEY(processing_id) REFERENCES "Processing" (id)
+);
+CREATE TABLE "Processing_volumes" (
+	"Processing_id" INTEGER, 
+	volumes_id INTEGER, 
+	PRIMARY KEY ("Processing_id", volumes_id), 
+	FOREIGN KEY("Processing_id") REFERENCES "Processing" (id), 
+	FOREIGN KEY(volumes_id) REFERENCES "Volumes" (id)
 );
 CREATE TABLE "EMDataset_grants" (
 	"EMDataset_id" INTEGER, 
